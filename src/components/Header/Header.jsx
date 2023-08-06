a35e5419949e326a24119164646d5a96efbc2190
myusername <chyharov@gmail.com>
2023-08-06 19:54:44 +0300

    ADDED: Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,20 @@
 import s from '../Header/Header.module.scss';
 
 const Header = () => {
-  return <div className={s.header}><h2 className='titleSemiBold'>React homework template</h2></div>;
+  return (
+    <header className={s.header}>
+      <div className={'container ' + s.header__container}>
+
+        <h2 className={s.nameLogo}>Event Planner</h2>
+
+        <select className={s.selectLanguage}>
+          <option value="option1">UA</option>
+          <option value="option2">EN</option>
+        </select>
+        
+      </div>
+    </header>
+  )
 };
 
 export default Header;

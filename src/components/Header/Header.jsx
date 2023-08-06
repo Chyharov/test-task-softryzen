c7247d1ea1b561c1426403673b752a70e9863c28
myusername <chyharov@gmail.com>
2023-08-06 21:57:52 +0300

    ADDED: search for header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,14 +3,23 @@ import s from '../Header/Header.module.scss';
 const Header = () => {
   return (
     <header className={s.header}>
-      <div className={'container ' + s.header__container}>
+      <div className={'container ' + s.header__padding}>
+        <div className={s.header__container}>
+          <h2 className={s.nameLogo}>Event Planner</h2>
 
-        <h2 className={s.nameLogo}>Event Planner</h2>
+          <select className={s.selectLanguage}>
+            <option value="option1">UA</option>
+            <option value="option2">EN</option>
+          </select>
+        </div>
 
-        <select className={s.selectLanguage}>
-          <option value="option1">UA</option>
-          <option value="option2">EN</option>
-        </select>
+        <form className={s.formSearch}>
+          <button className={s.btn__submit}>
+          </button>
+          <input className={s.inputSearch} type="text" placeholder="Search by keywords"/>
+        </form>
+
+        
         
       </div>
     </header>

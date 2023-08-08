3d4287ae8e7944f35f05a56df075f2a0cf656f52
myusername <chyharov@gmail.com>
2023-08-09 00:54:40 +0300

    FIX: sorted for event cards

--- src/components/Eventslist/Eventslist.jsx
+++ src/components/Eventslist/Eventslist.jsx
@@ -25,8 +25,6 @@ const Eventslist = () => {
     setShowModalSort(false);
   };
 
-  
-
   return (
     <section className={s.sectionEventList}>
     <div className={'container ' + s.event__container}>
@@ -85,27 +83,69 @@ const Eventslist = () => {
                   
                 <ul className={s.sort__list}>
                   <li className={s.sort__listItem}>
-                    <button className={s.sort__listBtn} onClick={() => {closeModalSort()}}>by name <svg width="24" height="24"><use href={`${icon}#icon-shapeup`}/></svg>
+                    <button className={s.sort__listBtn} onClick={() => { events.sort((a, b) => a.title.localeCompare(b.title)); closeModalSort(); }}
+                      >by name <svg width="24" height="24"><use href={`${icon}#icon-shapeup`}/></svg>
                     </button>
                   </li>
                   <li className={s.sort__listItem}>
-                    <button className={s.sort__listBtn} onClick={() => {closeModalSort()}}>by name <svg width="24" height="24"><use href={`${icon}#icon-shapedown`}/></svg>
+                    <button className={s.sort__listBtn} onClick={() => { events.sort((a, b) => b.title.localeCompare(a.title)); closeModalSort(); }}
+                      >by name <svg width="24" height="24"><use href={`${icon}#icon-shapedown`}/></svg>
                     </button>
                   </li>
                   <li className={s.sort__listItem}>
-                    <button className={s.sort__listBtn} onClick={() => {closeModalSort()}}>by data <svg width="24" height="24"><use href={`${icon}#icon-shapeup`}/></svg>
+                    <button className={s.sort__listBtn} onClick={() => {
+                      events.sort((a, b) => {
+                        const [dayA, monthA] = a.date.split('.').map(Number);
+                        const [dayB, monthB] = b.date.split('.').map(Number);
+
+                        const numerValueA = monthA * 100 + dayA;
+                        const numerValueB = monthB * 100 + dayB;
+
+                        return numerValueB - numerValueA;
+                      });
+                      closeModalSort();
+                    }}
+                  >by data <svg width="24" height="24"><use href={`${icon}#icon-shapeup`}/></svg>
                     </button>
                   </li>
                   <li className={s.sort__listItem}>
-                    <button className={s.sort__listBtn} onClick={() => {closeModalSort()}}>by data <svg width="24" height="24"><use href={`${icon}#icon-shapedown`}/></svg>
+                    <button className={s.sort__listBtn} onClick={() => {
+                      events.sort((a, b) => {
+                        const [dayA, monthA] = a.date.split('.').map(Number);
+                        const [dayB, monthB] = b.date.split('.').map(Number);
+
+                        const numerValueA = monthA * 100 + dayA;
+                        const numerValueB = monthB * 100 + dayB;
+
+                        return numerValueA - numerValueB;
+                      });
+                      closeModalSort();
+                    }}
+                  >by data <svg width="24" height="24"><use href={`${icon}#icon-shapedown`}/></svg>
                     </button>
                   </li>
                   <li className={s.sort__listItem}>
-                    <button className={s.sort__listBtn} onClick={() => {closeModalSort()}}>by priority <svg width="24" height="24"><use href={`${icon}#icon-shapeup`}/></svg>
+                    <button className={s.sort__listBtn} onClick={() => {
+                        events.sort((a, b) => {
+                          const priorityOrder = { "high": 3, "medium": 2, "low": 1 };
+
+                          return priorityOrder[b.priority.toLowerCase()] - priorityOrder[a.priority.toLowerCase()];
+                        });
+                        closeModalSort();
+                      }}
+                    >by priority <svg width="24" height="24"><use href={`${icon}#icon-shapeup`}/></svg>
                     </button>
                   </li>
                   <li className={s.sort__listItem}>
-                    <button className={s.sort__listBtn} onClick={() => {closeModalSort()}}>by priority <svg width="24" height="24"><use href={`${icon}#icon-shapedown`}/></svg>
+                    <button className={s.sort__listBtn} onClick={() => {
+                      events.sort((a, b) => {
+                        const priorityOrder = { "high": 3, "medium": 2, "low": 1 };
+
+                        return priorityOrder[a.priority.toLowerCase()] - priorityOrder[b.priority.toLowerCase()];
+                      });
+                      closeModalSort();
+                    }}
+                    >by priority <svg width="24" height="24"><use href={`${icon}#icon-shapedown`}/></svg>
                     </button>
                   </li>
                 </ul>

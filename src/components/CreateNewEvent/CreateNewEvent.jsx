83f7e15be3f49c5d832fca9e48dbd46565ddd98e
myusername <chyharov@gmail.com>
2023-08-09 06:42:58 +0300

    ADDED: README.md

--- src/components/CreateNewEvent/CreateNewEvent.jsx
+++ src/components/CreateNewEvent/CreateNewEvent.jsx
@@ -1,5 +1,6 @@
 import { Link } from 'react-router-dom';
 import EventForm from '../EventForm/EventForm';
+import icon from '../../images/icons/sprite.svg';
 import s from './CreateNewEvent.module.scss'
 
 const CreateNewEvent = () => {
@@ -9,7 +10,13 @@ const CreateNewEvent = () => {
         <section className={s.createNewEvent}>
             <div className={'container ' + s.event__container}>
 
-                <Link className={s.createNewEvent__linkBack} to="/">Back</Link>
+                <button className={s.button__back}>
+                    <Link className={s.createNewEvent__linkBack} to="/">
+                    <svg className={s.buttonsList__itemImg} width="24" height="24">
+                        <use href={`${icon}#icon-arrow-left`} />
+                    </svg>
+                    <p>Back</p></Link>
+                </button>
 
                 <h2 className={s.createNewEvent__Title}>Create new event</h2>
                 
--- src/components/EventForm/EventForm.jsx
+++ src/components/EventForm/EventForm.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import defaultPicture from '../../images/eventPictures/default.jpg'
 import s from './EventForm.module.scss';
 import { addNewEvent } from '../../services/Events.js';
@@ -9,52 +9,44 @@ const EventForm = () => {
   const [date, setDate] = useState('');
   const [time, setTime] = useState('');
   const [location, setLocation] = useState('');
-  const [category, setCategory] = useState('');
-  const [picture, setPicture] = useState('');
-  const [priority, setPriority] = useState('');
-  const [events, setEvents] = useState([]);
-
-  useEffect(() => {
-    const savedEvents = JSON.parse(localStorage.getItem('events')) || [];
-    setEvents(savedEvents);
-  }, []);
+  const [category, setCategory] = useState('Art');
+  const [priority, setPriority] = useState('High');
 
   const formatDate = (inputDate) => {
-    const [day, month, year] = inputDate.split('-');
-    return `${month}.${year}`;
+    const [, month, day] = inputDate.split('-');
+    return `${month}.${day}`;
   };
 
   const handleSubmit = (event) => {
-  event.preventDefault();
+    event.preventDefault();
 
-  const newEvent = {
-    id: new Date().getTime(),
-    title,
-    description,
-    date: formatDate(date),
-    time,
-    city: location,
-    category,
-    src: defaultPicture,
-    priority,
+    const newEvent = {
+      id: new Date().getTime(),
+      title,
+      description,
+      date: formatDate(date),
+      time,
+      city: location,
+      category,
+      src: defaultPicture,
+      priority,
     };
     
     addNewEvent(newEvent);
 
-  const existingEvents = JSON.parse(localStorage.getItem('events')) || [];
-  const updatedEvents = [...existingEvents, newEvent];
+    const existingEvents = JSON.parse(localStorage.getItem('events')) || [];
+    const updatedEvents = [...existingEvents, newEvent];
 
-  localStorage.setItem('events', JSON.stringify(updatedEvents));
+    localStorage.setItem('events', JSON.stringify(updatedEvents));
 
-  setTitle('');
-  setDescription('');
-  setDate('');
-  setTime('');
-  setLocation('');
-  setCategory('');
-  setPicture('');
-  setPriority('');
-};
+    setTitle('');
+    setDescription('');
+    setDate('');
+    setTime('');
+    setLocation('');
+    setCategory('Art');
+    setPriority('High');
+  };
 
   return (
     <form className={s.eventForm} onSubmit={handleSubmit}>
@@ -74,13 +66,25 @@ const EventForm = () => {
       <input className={s.form__input} type="text" value={location} onChange={(e) => setLocation(e.target.value)} />
 
       <h2 className={s.form__title}>Category</h2>
-      <input className={s.form__input} type="text" value={category} onChange={(e) => setCategory(e.target.value)} />
+      <select className={s.form__input} value={category} onChange={(e) => setCategory(e.target.value)}>
+        <option value="Art">Art</option>
+        <option value="Conference">Conference</option>
+        <option value="Workshop">Workshop</option>
+        <option value="Party">Party</option>
+        <option value="Business">Business</option>
+        <option value="Music">Music</option>
+        <option value="Sport">Sport</option>
+      </select>
 
       <h2 className={s.form__title}>Add picture</h2>
       <input className={s.form__input} type="text" value={defaultPicture} disabled />
 
       <h2 className={s.form__title}>Priority</h2>
-      <input className={s.form__input} type="text" value={priority} onChange={(e) => setPriority(e.target.value)} />
+      <select className={s.form__input} value={priority} onChange={(e) => setPriority(e.target.value)}>
+        <option value="High">High</option>
+        <option value="Medium">Medium</option>
+        <option value="Low">Low</option>
+      </select>
 
      <button className={s.form__submit} type="submit">Add Event</button>
     </form>
